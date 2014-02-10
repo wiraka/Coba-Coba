d66eaa8bb0a9cf5311fdca03db768d25f8ba5f54
wiraka <wiraka_ramah@yahoo.com>
2014-02-10 19:33:52 +0700

    last update

---
+++ index.php
@@ -0,0 +1,202 @@
+<html lang="en" class=" js flexbox canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths"><!--<![endif]--><head>
+<!-- Basic Page Needs
+	================================================== -->
+	<meta charset="utf-8">
+	<title>Asia Market Team</title>
+	<meta name="author" content="Little NEKO">
+<!-- Mobile Specific Metas
+	================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<!-- CSS
+	================================================== -->
+	<!-- Bootstrap  -->
+	<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
+	<!-- web font  -->
+	<link href="http://fonts.googleapis.com/css?family=Oswald:400,300" rel="stylesheet" type="text/css">
+	<!-- plugin css  -->
+	<link rel="stylesheet" type="text/css" href="css/animate.css">
+	<!-- Pop up-->
+	<link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+	<!-- Flex slider-->
+	<link rel="stylesheet" type="text/css" href="css/flexslider.css">
+	<!-- Owl carousel-->
+	<link rel="stylesheet" href="css/owl.carousel.css">
+	<link rel="stylesheet" href="css/owl.theme.css">
+	<!-- camera slider -->
+	<link type="text/css" rel="stylesheet" href="css/camera.css">
+	<!-- icon fonts -->
+	<link type="text/css" rel="stylesheet" href="css/custom-icons.css">
+	<link type="text/css" rel="stylesheet" href="css/custom-icons-ie7.css">
+	<!-- Custom css -->
+	<link type="text/css" rel="stylesheet" href="css/layout.css">
+	<link type="text/css" id="colors" rel="stylesheet" href="css/red.css">
+	<link type="text/css" rel="stylesheet" href="css/custom.css">
+	<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script> <![endif]-->
+	<script src="js/modernizr-2.6.1.min.js"></script>
+    <script src="http://code.jquery.com/jquery.js"></script>
+	<script src="src/skdslider.min.js"></script>
+	<link href="src/skdslider.css" rel="stylesheet">
+	<script type="text/javascript">
+		jQuery(document).ready(function(){
+			jQuery('#demo1').skdslider({delay:5000, animationSpeed: 2000,showNextPrev:true,showPlayButton:true,autoSlide:true,animationType:'fading'});
+			jQuery('#demo2').skdslider({delay:5000, animationSpeed: 1000,showNextPrev:true,showPlayButton:false,autoSlide:true,animationType:'sliding'});
+			jQuery('#demo3').skdslider({delay:5000, animationSpeed: 2000,showNextPrev:true,showPlayButton:true,autoSlide:true,animationType:'fading'});
+			
+			jQuery('#responsive').change(function(){
+			  $('#responsive_wrapper').width(jQuery(this).val());
+			});
+			
+		});
+	</script>
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-7175253790862528";
+/* SKDSlider */
+google_ad_slot = "4030515492";
+google_ad_width = 468;
+google_ad_height = 60;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-7175253790862528";
+/* SKDSlider */
+google_ad_slot = "4030515492";
+google_ad_width = 468;
+google_ad_height = 60;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-3415878-12', 'dandywebsolution.com');
+  ga('send', 'pageview');
+
+</script>
+<!-- Favicons
+	================================================== -->
+<style>.foicli {                    position:relative;                    z-index:1;                    overflow:hidden;                    list-style:none;                    padding:0;                    margin:0 0 0.25em;                }                .foicli span:link,                    .foicli span:visited {                    display:block;                    border:0;                    padding-left:28px;                    color:#aaa;                }                .foicli span:hover,                    .foicli span:focus,                    .foicli span:active {                    color:#730800;                    background:transparent;                }                .foicli:before,                    .foicli:after,                    .foicli span:before,                    .foicli span:after {                    content:'';                    position:absolute;                    top:50%;                    left:0;                }                .foicli span:before,                    .foicli span:after {                    margin:-8px 0 0;                    background:#aaa;                }                .foicli span:hover:before,                    .foicli span:focus:before,                    .foicli span:active:before {                    background:#730800;                }                .tools{                    height:20px;                    cursor: pointer;                }                .tools:after {                    left:13px;                    width:3px;                    height:5px;                    margin-top:-8px;                    background:#fff;                    /* css3 */                    -webkit-transform:rotate(45deg);                    -moz-transform:rotate(45deg);                    -ms-transform:rotate(45deg);                    -o-transform:rotate(45deg);                    transform:rotate(45deg);                }                .tools span:before {                    left:6px;                    width:4px;                    height:15px;                    margin-top:-7px;                    background:#aaa;                    /* css3 */                    -webkit-transform:rotate(45deg);                    -moz-transform:rotate(45deg);                    -ms-transform:rotate(45deg);                    -o-transform:rotate(45deg);                    transform:rotate(45deg);                }                .tools span:after {                    left:8px;                    width:9px;                    height:9px;                    background:#aaa;                    -webkit-border-radius:8px;                    -moz-border-radius:8px;                    border-radius:8px;                }                .tools span:hover:after,.tools span:focus:after,.tools span:active:after{                    background:#730800;                }                .power{                    height:16px;                    cursor: pointer;                }                .power span:before {                    left:1px;                    width:10px;                    height:10px;                    border:2px solid #aaa;                    margin-top:-6px;                    background:transparent;                    /* css3 */                    -webkit-border-radius:16px;                    -moz-border-radius:16px;                    border-radius:16px;                }                .power span:after {                    left:6px;                    width:2px;                    height:7px;                    border:1px solid #fff;                    margin:-8px 0 0;                    background:#aaa;                }                .power span:hover:before,                .power span:focus:before,                .power span:active:before {                    border-color:#730800;                    background:transparent;                }                .power span:hover:after,                .power span:focus:after,                .power span:active:after {                    background:#730800;                }</style><style type="text/css">.q8pva242bk6o {  display:inline !important; list-style:none; text-align:left; float:none; padding:0; margin:0; border:1px solid transparent; border-bottom:1px solid; text-decoration:underline; color:#009900; font-weight:normal; cursor:pointer; }</style><style type="text/css">.adbox  { padding:0 !important; margin:0 !important; display:block; xwidth:400px; xheight:200px;  z-index:2e9; text-decoration:none !important;}</style></head>
+<body class="header1">
+	<ul id="demo1">
+	<li>
+	<img src="slides/1.jpg" />
+	<!--Slider Description example-->
+	</li>
+	<li><img src="slides/2.jpg" />
+	</li>
+	<li><img src="slides/3.jpg" />
+	<!--NO Description Here-->
+	</li>
+	<li><img src="slides/4.jpg" />
+	</li>
+	</ul>
+    <div>
+	<div id="globalWrapper" class="localscroll">
+		<section id="content">
+                            <!-- parallax testimonial --> 
+							<section id="paralaxSlice5" data-stellar-background-ratio="0.5">
+								<div class="maskParent">
+									<div class="paralaxMask"></div>
+									<div class="paralaxText container">
+										<div class="row">
+											<div class="col-md-12">
+												<div id="carouselWorks1" class="owl-carousel noOwlButtons testimonialCarousel owl-theme" style="opacity: 1; display: block;">
+
+													<div class="owl-wrapper-outer"><div class="owl-wrapper" style="width: 6840px; left: 0px; display: block;"><div class="owl-item" style="width: 1140px;"><div class="item">
+														<img src="images/clients/client1.jpg" class="img-circle mb15" alt="client">
+														<blockquote> When you are courting a nice girl an hour seems like a second.<br> When you sit on a red-hot cinder a second seems like an hour. That's relativity.<br><small>Albert Einstein </small></blockquote>
+													</div></div></div></div>
+												<div class="owl-controls clickable"><div class="owl-pagination"><div class="owl-page active"><span class=""></span></div><div class="owl-page"><span class=""></span></div><div class="owl-page"><span class=""></span></div></div><div class="owl-buttons"><div class="owl-prev"><i class="icon-left-open"></i></div><div class="owl-next"><i class="icon-right-open"></i></div></div></div></div>
+
+											</div>
+										</div>
+									</div>
+								</div>
+							</section>
+							<!-- parallax testimonial --> 
+							<!-- team -->
+							<section id="team">
+								<div class="title">
+									<h1>Hello world</h1>
+									<h2 class="subTitle">introduce our great team</h2>
+								</div>
+								<section class="pt30 pb30">
+									<div class="container">
+										<div class="row"> 
+											<div class="col-sm-3">
+												<article>
+													<div><img src="images/team/pic1.jpg" alt="" class="img-responsive"></div>
+													<div class="boxContent color2">
+														<h3>Faruq Ahmad Faishal</h3>
+														<p>Direktur Utama Asia Market</p>
+													</div>
+												</article>
+											</div>
+											<div class="col-sm-3">
+												<article>
+													<div><img src="images/team/pic2.jpg" alt="" class="img-responsive"></div>
+													<div class="boxContent color2">
+														<h3>Hafidz Ali Maulana</h3>
+														<p>Creative Director</p>
+													</div>
+												</article>
+											</div>
+											<div class="col-sm-3">
+												<article>
+													<div><img src="images/team/pic3.jpg" alt="" class="img-responsive"></div>
+													<div class="boxContent color2">
+														<h3>Wiraka yuniarto</h3>
+														<p>Marketing</p>
+													</div>
+												</article>
+											</div>
+											<div class="col-sm-3">
+												<article>
+													<div><img src="images/team/pic4.jpg" alt="" class="img-responsive"></div>
+													<div class="boxContent color2">
+														<h3>Moch Arif Hidayat</h3>
+														<p>Programmer</p>
+													</div>
+												</article>
+											</div>
+										</div>
+									</div>
+								</section>
+							</section>
+							<!-- team -->
+                            <!-- parallax testimonial --> 
+							<section id="paralaxSlice5" data-stellar-background-ratio="0.5">
+								<div class="maskParent">
+									<div class="paralaxMask"></div>
+									<div class="paralaxText container">
+										<div class="row">
+											<div class="col-md-12">
+												<div id="carouselWorks1" class="owl-carousel noOwlButtons testimonialCarousel owl-theme" style="opacity: 1; display: block;">
+
+													<div class="owl-wrapper-outer"><div class="owl-wrapper" style="width: 6840px; left: 0px; display: block;"><div class="owl-item" style="width: 1140px;"><div class="item">
+														<img src="images/clients/client3.jpg" class="img-circle mb15" alt="client">
+														<blockquote>Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma - which is living with the results of other people's thinking. Don't let the noise of others' opinions drown out your own inner voice. And most important, have the courage to follow your heart and intuition.<br><small>Steve Jobs</small></blockquote>
+													</div></div></div></div>
+												<div class="owl-controls clickable"><div class="owl-pagination"><div class="owl-page active"><span class=""></span></div><div class="owl-page"><span class=""></span></div><div class="owl-page"><span class=""></span></div></div><div class="owl-buttons"><div class="owl-prev"><i class="icon-left-open"></i></div><div class="owl-next"><i class="icon-right-open"></i></div></div></div></div>
+
+											</div>
+										</div>
+									</div>
+								</div>
+							</section>
+							<!-- parallax testimonial -->
+						<!-- content -->
+					</section>
+				</div>
+				<!-- global wrapper -->
+<!-- End Document 
+	================================================== -->
+	
+</body></html>
